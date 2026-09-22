703076383009b10c88038abbe9562f6fe97b5bca
yx1624418480-debug <yx1624418480@gmail.com>
2026-09-23 01:41:19 +0800

    Update next.config.ts
    添加静态导出配置

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,12 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
+    output: 'export',
   // outputFileTracingRoot: path.resolve(__dirname, '../../'),  // Uncomment and add 'import path from "path"' if needed
   /* config options here */
   allowedDevOrigins: ['*.dev.coze.site'],
   images: {
+        unoptimized: true,
     remotePatterns: [
       {
         protocol: 'https',
